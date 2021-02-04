99d260d4935181843e1979e1e6899c8de9918f38
Halston <sellenth@oregonstate.edu>
2021-02-04 01:10:23 -0800

    says hi...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Heading, Container } from "@chakra-ui/react"
 
-function App() {
+function oscillate(currText, stateFunc) {
+  let count = 0;
+  let intervalId = setInterval(function () {
+    if (count === 0) {
+      stateFunc(<>{currText}<span style={{opacity: "0"}}>.</span><span style={{opacity: "0"}}>.</span><span style={{opacity: "0"}}>.</span></>);
+    } else if (count === 1) {
+      stateFunc(<>{currText}<span style={{opacity: "1"}}>.</span><span style={{opacity: "0"}}>.</span><span style={{opacity: "0"}}>.</span></>);
+    } else if (count === 2) {
+      stateFunc(<>{currText}<span style={{opacity: "1"}}>.</span><span style={{opacity: "1"}}>.</span><span style={{opacity: "0"}}>.</span></>);
+    } else if (count === 3) {
+      stateFunc(<>{currText}<span style={{opacity: "1"}}>.</span><span style={{opacity: "1"}}>.</span><span style={{opacity: "1"}}>.</span></>);
+    } else if (count === 4) {
+      clearInterval(intervalId);
+    }
+    count += 1
+  }, 500);
+}
+
+const App = () => {
+  const [headingText, setHeadingText] = React.useState();
+  React.useEffect(() => {
+    oscillate('Hi', setHeadingText)
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Heading whiteSpace="pre-wrap" size="4xl">{headingText}</Heading>
   );
 }
 
